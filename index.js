dc4aa21334c9976acdbfb735e2704d6121e2ea5e
shyamprgrmr <2018bcs509@sggs.ac.in>
2024-07-14 16:45:07 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 var server_name = process.env.SERVER_NAME;
-console.log(server_name)
+console.log("Server Name : " + server_name)
 app.get('/application', function (req, res) {
     res.status(200).send("Response received from : "+server_name); 
 });
