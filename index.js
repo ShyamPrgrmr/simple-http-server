123186063b37bdaeab1ab63da7057a08d553d701
shyamprgrmr <2018bcs509@sggs.ac.in>
2024-09-20 21:44:01 +0530

    A simple node app to check get, post and has /healthcheck url in it

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var app = express();
 require('dotenv').config();
-var server_name = process.env.SERVER_NAME;
+
+app.use(express.json());
+
+var server_name = process.env.SERVER_NAME+":"+process.env.S_PORT;
+
 console.log("Server Name : " + server_name)
+
 app.get('/application', function (req, res) {
-    res.status(200).send("Response received from : "+server_name); 
+    console.log("GET Request Received");
+    console.log("Request ID : "+ req.headers.request_id);
+    res.status(200).send("GET Response received from : "+server_name); 
 });
-app.listen(3000, function () {console.log('Example app listening on port 3000!');});
+
+app.post('/application', function (req, res) {
+    console.log("POST Request Received");
+    console.log("Request ID : "+ req.headers.request_id);    
+    res.status(200).json({"greetings":"hello from server","request body":req.body});
+
+});
+
+
+app.get("/healthcheck",(req,res)=>{
+    //logic may be db connection check
+    console.log("Healthcheck Request Received");
+    res.status(200).json({"Status":"Green"});  
+}); 
+
+
+app.listen( process.env.S_PORT || 3000, function () {console.log(server_name + ' listening on port '+process.env.S_PORT);});
